905703f9d357c9442e259ae49b4c6b39666efc5e
Ignacio Oyarzabal <ignaoya93@gmail.com>
2019-03-05 14:34:40 +0100

    Added more tests

--- tests.py
+++ tests.py
@@ -15,12 +15,61 @@ class TestGetMazeId(unittest.TestCase):
 class TestGetMazeState(unittest.TestCase):
 
     def setUp(self):
-        request = get_maze_id(url, data)
-        maze_url = '/'.join([url, request.json()['maze_id']])
-        self.maze = get_maze_state(maze_url)
+        self.request = get_maze_id(url, data)
+        self.maze_url = '/'.join([url, self.request.json()['maze_id']])
+        self.maze = get_maze_state(self.maze_url)
     
-    def test_status_code_200(self):
-        self.assertEqual(self.maze['game-state']['state'], 'Active', f"active is not {self.maze['game-state']}")
+    def test_state_active(self):
+        self.assertEqual(self.maze['game-state']['state'], 'Active', f"Active is not {self.maze['game-state']['state']}")
+
+    def test_pony_not_in_endpoint(self):
+        self.assertNotEqual(self.maze['pony'], self.maze['end-point'])
+
+class TestGetMoves(unittest.TestCase):
+
+    def setUp(self):
+        self.maze = {'data':[['north','west'],['north','west'],['west'],[]]}
+        self.width = 2
+
+    def test_west(self):
+        pos_a = 3
+        pos_b = 2
+
+        moves_a = get_moves(pos_a, self.maze, self.width)
+        moves_b = get_moves(pos_b, self.maze, self.width)
+
+        self.assertIn('west', moves_a)
+        self.assertNotIn('west', moves_b)
+
+    def test_north(self):
+        pos_a = 2
+        pos_b = 0
+
+        moves_a = get_moves(pos_a, self.maze, self.width)
+        moves_b = get_moves(pos_b, self.maze, self.width)
+
+        self.assertIn('north', moves_a)
+        self.assertNotIn('north', moves_b)
+
+    def test_east(self):
+        pos_a = 2
+        pos_b = 0
+
+        moves_a = get_moves(pos_a, self.maze, self.width)
+        moves_b = get_moves(pos_b, self.maze, self.width)
+
+        self.assertIn('east', moves_a)
+        self.assertNotIn('east', moves_b)
+
+    def test_south(self):
+        pos_a = 0
+        pos_b = 2
+
+        moves_a = get_moves(pos_a, self.maze, self.width)
+        moves_b = get_moves(pos_b, self.maze, self.width)
+
+        self.assertIn('south', moves_a)
+        self.assertNotIn('south', moves_b)
 
 if __name__ == '__main__':
     unittest.main()
